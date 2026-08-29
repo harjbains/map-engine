f3d06b5abeb299ee19f7d3b890ea234e76236c9c
harjbains <harjbains@gmail.com>
2026-08-29 15:21:10 +0100

    v2.4.3: raise corridor fetch cap to 18s so slow Overpass mirrors still yield route options

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "map-engine-shell-v1173";
+const SHELL_CACHE = "map-engine-shell-v1174";
 const MAP_CACHE = "map-engine-map-v1";
 
 const scopeUrl = new URL(self.registration.scope);
