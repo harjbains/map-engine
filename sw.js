9953b9b3b101a7209dc74996dd55e780764c97e7
harjbains <harjbains@gmail.com>
2026-08-29 12:12:15 +0100

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "map-engine-shell-v1166";
+const SHELL_CACHE = "map-engine-shell-v1167";
 const MAP_CACHE = "map-engine-map-v1";
 
 const scopeUrl = new URL(self.registration.scope);
