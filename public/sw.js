c727569c8db8378e877ce42f6e941c85f727b54e
harjbains <harjbains@gmail.com>
2026-09-09 17:17:38 +0100

    v2.10.64 - raise the Re-centre button when it is the only drive control so it clears the Enter Experimental Driver View toggle on the Tesla browser

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "map-engine-shell-v1245";
+const SHELL_CACHE = "map-engine-shell-v1246";
 const MAP_CACHE = "map-engine-map-v1";
 
 const scopeUrl = new URL(self.registration.scope);
