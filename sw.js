318d87eada82ee1a523cd6e1c140b5bcb233670b
harjbains <harjbains@gmail.com>
2026-09-11 13:46:00 +0100

    v2.10.92 ending a shift resets today's totals and rides (sw v1274)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "map-engine-shell-v1273";
+const SHELL_CACHE = "map-engine-shell-v1274";
 const MAP_CACHE = "map-engine-map-v1";
 
 const scopeUrl = new URL(self.registration.scope);
