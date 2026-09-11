a3cc9bc70fc97aabbcde4b9c745b0c82d92c70ef
harjbains <harjbains@gmail.com>
2026-09-11 16:59:47 +0100

    v2.10.94 Celebration V2: extravagant £25 milestone animation (sw v1276)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "map-engine-shell-v1275";
+const SHELL_CACHE = "map-engine-shell-v1276";
 const MAP_CACHE = "map-engine-map-v1";
 
 const scopeUrl = new URL(self.registration.scope);
