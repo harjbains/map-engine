06dfe24d6e124a1d41d85ccb550681ca89b6310f
harjbains <harjbains@gmail.com>
2026-09-13 17:18:25 +0100

    v2.10.95 mileage input (sw v1277)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SHELL_CACHE = "map-engine-shell-v1276";
+const SHELL_CACHE = "map-engine-shell-v1277";
 const MAP_CACHE = "map-engine-map-v1";
 
 const scopeUrl = new URL(self.registration.scope);
